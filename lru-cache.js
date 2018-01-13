cd41a2da3ad787e8fc8ba89a45269756603867c4
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-01-13 18:03:06 -0500

    formatting

--- lru-cache.js
+++ lru-cache.js
@@ -1,13 +1,10 @@
 // original license:
 // 
 // The ISC License
-
 // Copyright (c) Isaac Z. Schlueter and Contributors
-
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
 // copyright notice and this permission notice appear in all copies.
-
 // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 // WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -16,394 +13,408 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
 // IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+window.LRUCache = (function () {
+	'use strict';
 
-'use strict'
+	// use symbols if possible, otherwise just _props
+	var hasSymbol = typeof Symbol === 'function';
+	var makeSymbol;
+	if (hasSymbol) {
+		makeSymbol = function (key) {
+			return Symbol.for(key);
+		};
+	} else {
+		makeSymbol = function (key) {
+			return '_' + key;
+		};
+	}
 
-window.LRUCache = (function(){
+	var MAX = makeSymbol('max');
+	var LENGTH = makeSymbol('length');
+	var LENGTH_CALCULATOR = makeSymbol('lengthCalculator');
+	var ALLOW_STALE = makeSymbol('allowStale');
+	var MAX_AGE = makeSymbol('maxAge');
+	var DISPOSE = makeSymbol('dispose');
+	var NO_DISPOSE_ON_SET = makeSymbol('noDisposeOnSet');
+	var LRU_LIST = makeSymbol('lruList');
+	var CACHE = makeSymbol('cache');
 
-// use symbols if possible, otherwise just _props
-var hasSymbol = typeof Symbol === 'function'
-var makeSymbol
-if (hasSymbol) {
-  makeSymbol = function (key) {
-    return Symbol.for(key)
-  }
-} else {
-  makeSymbol = function (key) {
-    return '_' + key
-  }
-}
+	function naiveLength() {
+		return 1;
+	}
 
-var MAX = makeSymbol('max')
-var LENGTH = makeSymbol('length')
-var LENGTH_CALCULATOR = makeSymbol('lengthCalculator')
-var ALLOW_STALE = makeSymbol('allowStale')
-var MAX_AGE = makeSymbol('maxAge')
-var DISPOSE = makeSymbol('dispose')
-var NO_DISPOSE_ON_SET = makeSymbol('noDisposeOnSet')
-var LRU_LIST = makeSymbol('lruList')
-var CACHE = makeSymbol('cache')
+	// lruList is a yallist where the head is the youngest
+	// item, and the tail is the oldest.  the list contains the Hit
+	// objects as the entries.
+	// Each Hit object has a reference to its Yallist.Node.  This
+	// never changes.
+	//
+	// cache is a Map (or PseudoMap) that matches the keys to
+	// the Yallist.Node object.
+	function LRUCache(options) {
+		if (!(this instanceof LRUCache)) {
+			return new LRUCache(options);
+		}
 
-function naiveLength () { return 1 }
+		if (typeof options === 'number') {
+			options = {
+				max: options
+			};
+		}
 
-// lruList is a yallist where the head is the youngest
-// item, and the tail is the oldest.  the list contains the Hit
-// objects as the entries.
-// Each Hit object has a reference to its Yallist.Node.  This
-// never changes.
-//
-// cache is a Map (or PseudoMap) that matches the keys to
-// the Yallist.Node object.
-function LRUCache (options) {
-  if (!(this instanceof LRUCache)) {
-    return new LRUCache(options)
-  }
+		if (!options) {
+			options = {};
+		}
 
-  if (typeof options === 'number') {
-    options = { max: options }
-  }
+		var max = this[MAX] = options.max;
+		// Kind of weird to have a default max of Infinity, but oh well.
+		if (!max || typeof max !== 'number' || max <= 0) {
+			this[MAX] = Infinity;
+		}
 
-  if (!options) {
-    options = {}
-  }
+		var lc = options.length || naiveLength;
+		if (typeof lc !== 'function') {
+			lc = naiveLength;
+		}
+		this[LENGTH_CALCULATOR] = lc;
 
-  var max = this[MAX] = options.max
-  // Kind of weird to have a default max of Infinity, but oh well.
-  if (!max ||
-      !(typeof max === 'number') ||
-      max <= 0) {
-    this[MAX] = Infinity
-  }
+		this[ALLOW_STALE] = options.stale || false;
+		this[MAX_AGE] = options.maxAge || 0;
+		this[DISPOSE] = options.dispose;
+		this[NO_DISPOSE_ON_SET] = options.noDisposeOnSet || false;
+		this.reset();
+	}
 
-  var lc = options.length || naiveLength
-  if (typeof lc !== 'function') {
-    lc = naiveLength
-  }
-  this[LENGTH_CALCULATOR] = lc
+	// resize the cache when the max changes.
+	Object.defineProperty(LRUCache.prototype, 'max', {
+		set: function (mL) {
+			if (!mL || typeof mL !== 'number' || mL <= 0) {
+				mL = Infinity;
+			}
+			this[MAX] = mL;
+			trim(this);
+		},
+		get: function () {
+			return this[MAX];
+		},
+		enumerable: true
+	});
 
-  this[ALLOW_STALE] = options.stale || false
-  this[MAX_AGE] = options.maxAge || 0
-  this[DISPOSE] = options.dispose
-  this[NO_DISPOSE_ON_SET] = options.noDisposeOnSet || false
-  this.reset()
-}
+	Object.defineProperty(LRUCache.prototype, 'allowStale', {
+		set: function (allowStale) {
+			this[ALLOW_STALE] = !!allowStale;
+		},
+		get: function () {
+			return this[ALLOW_STALE];
+		},
+		enumerable: true
+	});
 
-// resize the cache when the max changes.
-Object.defineProperty(LRUCache.prototype, 'max', {
-  set: function (mL) {
-    if (!mL || !(typeof mL === 'number') || mL <= 0) {
-      mL = Infinity
-    }
-    this[MAX] = mL
-    trim(this)
-  },
-  get: function () {
-    return this[MAX]
-  },
-  enumerable: true
-})
+	Object.defineProperty(LRUCache.prototype, 'maxAge', {
+		set: function (mA) {
+			if (!mA || typeof mA !== 'number' || mA < 0) {
+				mA = 0;
+			}
+			this[MAX_AGE] = mA;
+			trim(this);
+		},
+		get: function () {
+			return this[MAX_AGE];
+		},
+		enumerable: true
+	});
 
-Object.defineProperty(LRUCache.prototype, 'allowStale', {
-  set: function (allowStale) {
-    this[ALLOW_STALE] = !!allowStale
-  },
-  get: function () {
-    return this[ALLOW_STALE]
-  },
-  enumerable: true
-})
+	// resize the cache when the lengthCalculator changes.
+	Object.defineProperty(LRUCache.prototype, 'lengthCalculator', {
+		set: function (lC) {
+			if (typeof lC !== 'function') {
+				lC = naiveLength;
+			}
+			if (lC !== this[LENGTH_CALCULATOR]) {
+				this[LENGTH_CALCULATOR] = lC;
+				this[LENGTH] = 0;
+				this[LRU_LIST].forEach(function (hit) {
+					hit.length = this[LENGTH_CALCULATOR](hit.value, hit.key);
+					this[LENGTH] += hit.length;
+				}, this);
+			}
+			trim(this);
+		},
+		get: function () {
+			return this[LENGTH_CALCULATOR];
+		},
+		enumerable: true
+	});
 
-Object.defineProperty(LRUCache.prototype, 'maxAge', {
-  set: function (mA) {
-    if (!mA || !(typeof mA === 'number') || mA < 0) {
-      mA = 0
-    }
-    this[MAX_AGE] = mA
-    trim(this)
-  },
-  get: function () {
-    return this[MAX_AGE]
-  },
-  enumerable: true
-})
+	Object.defineProperty(LRUCache.prototype, 'length', {
+		get: function () {
+			return this[LENGTH];
+		},
+		enumerable: true
+	});
 
-// resize the cache when the lengthCalculator changes.
-Object.defineProperty(LRUCache.prototype, 'lengthCalculator', {
-  set: function (lC) {
-    if (typeof lC !== 'function') {
-      lC = naiveLength
-    }
-    if (lC !== this[LENGTH_CALCULATOR]) {
-      this[LENGTH_CALCULATOR] = lC
-      this[LENGTH] = 0
-      this[LRU_LIST].forEach(function (hit) {
-        hit.length = this[LENGTH_CALCULATOR](hit.value, hit.key)
-        this[LENGTH] += hit.length
-      }, this)
-    }
-    trim(this)
-  },
-  get: function () { return this[LENGTH_CALCULATOR] },
-  enumerable: true
-})
+	Object.defineProperty(LRUCache.prototype, 'itemCount', {
+		get: function () {
+			return this[LRU_LIST].length;
+		},
+		enumerable: true
+	});
 
-Object.defineProperty(LRUCache.prototype, 'length', {
-  get: function () { return this[LENGTH] },
-  enumerable: true
-})
+	LRUCache.prototype.rforEach = function (fn, thisp) {
+		thisp = thisp || this;
+		for (var walker = this[LRU_LIST].tail; walker !== null;) {
+			var prev = walker.prev;
+			forEachStep(this, fn, walker, thisp);
+			walker = prev;
+		}
+	};
 
-Object.defineProperty(LRUCache.prototype, 'itemCount', {
-  get: function () { return this[LRU_LIST].length },
-  enumerable: true
-})
+	function forEachStep(self, fn, node, thisp) {
+		var hit = node.value;
+		if (isStale(self, hit)) {
+			del(self, node);
+			if (!self[ALLOW_STALE]) {
+				hit = undefined;
+			}
+		}
+		if (hit) {
+			fn.call(thisp, hit.value, hit.key, self);
+		}
+	}
 
-LRUCache.prototype.rforEach = function (fn, thisp) {
-  thisp = thisp || this
-  for (var walker = this[LRU_LIST].tail; walker !== null;) {
-    var prev = walker.prev
-    forEachStep(this, fn, walker, thisp)
-    walker = prev
-  }
-}
+	LRUCache.prototype.forEach = function (fn, thisp) {
+		thisp = thisp || this;
+		for (var walker = this[LRU_LIST].head; walker !== null;) {
+			var next = walker.next;
+			forEachStep(this, fn, walker, thisp);
+			walker = next;
+		}
+	};
 
-function forEachStep (self, fn, node, thisp) {
-  var hit = node.value
-  if (isStale(self, hit)) {
-    del(self, node)
-    if (!self[ALLOW_STALE]) {
-      hit = undefined
-    }
-  }
-  if (hit) {
-    fn.call(thisp, hit.value, hit.key, self)
-  }
-}
+	LRUCache.prototype.keys = function () {
+		return this[LRU_LIST].toArray().map(function (k) {
+			return k.key;
+		}, this);
+	};
 
-LRUCache.prototype.forEach = function (fn, thisp) {
-  thisp = thisp || this
-  for (var walker = this[LRU_LIST].head; walker !== null;) {
-    var next = walker.next
-    forEachStep(this, fn, walker, thisp)
-    walker = next
-  }
-}
+	LRUCache.prototype.values = function () {
+		return this[LRU_LIST].toArray().map(function (k) {
+			return k.value;
+		}, this);
+	};
 
-LRUCache.prototype.keys = function () {
-  return this[LRU_LIST].toArray().map(function (k) {
-    return k.key
-  }, this)
-}
+	LRUCache.prototype.reset = function () {
+		if (this[DISPOSE] &&
+			this[LRU_LIST] &&
+			this[LRU_LIST].length) {
+			this[LRU_LIST].forEach(function (hit) {
+				this[DISPOSE](hit.key, hit.value);
+			}, this);
+		}
 
-LRUCache.prototype.values = function () {
-  return this[LRU_LIST].toArray().map(function (k) {
-    return k.value
-  }, this)
-}
+		this[CACHE] = new Map(); // hash of items by key
+		this[LRU_LIST] = new Yallist(); // list of items in order of use recency
+		this[LENGTH] = 0; // length of items in the list
+	};
 
-LRUCache.prototype.reset = function () {
-  if (this[DISPOSE] &&
-      this[LRU_LIST] &&
-      this[LRU_LIST].length) {
-    this[LRU_LIST].forEach(function (hit) {
-      this[DISPOSE](hit.key, hit.value)
-    }, this)
-  }
+	LRUCache.prototype.dump = function () {
+		return this[LRU_LIST].map(function (hit) {
+			if (!isStale(this, hit)) {
+				return {
+					k: hit.key,
+					v: hit.value,
+					e: hit.now + (hit.maxAge || 0)
+				};
+			}
+		}, this).toArray().filter(function (h) {
+			return h;
+		});
+	};
 
-  this[CACHE] = new Map() // hash of items by key
-  this[LRU_LIST] = new Yallist() // list of items in order of use recency
-  this[LENGTH] = 0 // length of items in the list
-}
+	LRUCache.prototype.dumpLru = function () {
+		return this[LRU_LIST];
+	};
 
-LRUCache.prototype.dump = function () {
-  return this[LRU_LIST].map(function (hit) {
-    if (!isStale(this, hit)) {
-      return {
-        k: hit.key,
-        v: hit.value,
-        e: hit.now + (hit.maxAge || 0)
-      }
-    }
-  }, this).toArray().filter(function (h) {
-    return h
-  })
-}
+	LRUCache.prototype.set = function (key, value, maxAge) {
+		maxAge = maxAge || this[MAX_AGE];
 
-LRUCache.prototype.dumpLru = function () {
-  return this[LRU_LIST]
-}
+		var now = maxAge ? Date.now() : 0;
+		var len = this[LENGTH_CALCULATOR](value, key);
 
-LRUCache.prototype.set = function (key, value, maxAge) {
-  maxAge = maxAge || this[MAX_AGE]
+		if (this[CACHE].has(key)) {
+			if (len > this[MAX]) {
+				del(this, this[CACHE].get(key));
+				return false;
+			}
 
-  var now = maxAge ? Date.now() : 0
-  var len = this[LENGTH_CALCULATOR](value, key)
+			var node = this[CACHE].get(key);
+			var item = node.value;
 
-  if (this[CACHE].has(key)) {
-    if (len > this[MAX]) {
-      del(this, this[CACHE].get(key))
-      return false
-    }
+			// dispose of the old one before overwriting
+			// split out into 2 ifs for better coverage tracking
+			if (this[DISPOSE]) {
+				if (!this[NO_DISPOSE_ON_SET]) {
+					this[DISPOSE](key, item.value);
+				}
+			}
 
-    var node = this[CACHE].get(key)
-    var item = node.value
+			item.now = now;
+			item.maxAge = maxAge;
+			item.value = value;
+			this[LENGTH] += len - item.length;
+			item.length = len;
+			this.get(key);
+			trim(this);
+			return true;
+		}
 
-    // dispose of the old one before overwriting
-    // split out into 2 ifs for better coverage tracking
-    if (this[DISPOSE]) {
-      if (!this[NO_DISPOSE_ON_SET]) {
-        this[DISPOSE](key, item.value)
-      }
-    }
+		var hit = new Entry(key, value, len, now, maxAge);
 
-    item.now = now
-    item.maxAge = maxAge
-    item.value = value
-    this[LENGTH] += len - item.length
-    item.length = len
-    this.get(key)
-    trim(this)
-    return true
-  }
+		// oversized objects fall out of cache automatically.
+		if (hit.length > this[MAX]) {
+			if (this[DISPOSE]) {
+				this[DISPOSE](key, value);
+			}
+			return false;
+		}
 
-  var hit = new Entry(key, value, len, now, maxAge)
+		this[LENGTH] += hit.length;
+		this[LRU_LIST].unshift(hit);
+		this[CACHE].set(key, this[LRU_LIST].head);
+		trim(this);
+		return true;
+	};
 
-  // oversized objects fall out of cache automatically.
-  if (hit.length > this[MAX]) {
-    if (this[DISPOSE]) {
-      this[DISPOSE](key, value)
-    }
-    return false
-  }
+	LRUCache.prototype.has = function (key) {
+		if (!this[CACHE].has(key)) {
+			return false;
+		}
+		var hit = this[CACHE].get(key).value;
+		if (isStale(this, hit)) {
+			return false;
+		}
+		return true;
+	};
 
-  this[LENGTH] += hit.length
-  this[LRU_LIST].unshift(hit)
-  this[CACHE].set(key, this[LRU_LIST].head)
-  trim(this)
-  return true
-}
+	LRUCache.prototype.get = function (key) {
+		return get(this, key, true);
+	};
 
-LRUCache.prototype.has = function (key) {
-  if (!this[CACHE].has(key)) return false
-  var hit = this[CACHE].get(key).value
-  if (isStale(this, hit)) {
-    return false
-  }
-  return true
-}
+	LRUCache.prototype.peek = function (key) {
+		return get(this, key, false);
+	};
 
-LRUCache.prototype.get = function (key) {
-  return get(this, key, true)
-}
+	LRUCache.prototype.pop = function () {
+		var node = this[LRU_LIST].tail;
+		if (!node) {
+			return null;
+		}
+		del(this, node);
+		return node.value;
+	};
 
-LRUCache.prototype.peek = function (key) {
-  return get(this, key, false)
-}
+	LRUCache.prototype.del = function (key) {
+		del(this, this[CACHE].get(key));
+	};
 
-LRUCache.prototype.pop = function () {
-  var node = this[LRU_LIST].tail
-  if (!node) return null
-  del(this, node)
-  return node.value
-}
+	LRUCache.prototype.load = function (arr) {
+		// reset the cache
+		this.reset();
 
-LRUCache.prototype.del = function (key) {
-  del(this, this[CACHE].get(key))
-}
+		var now = Date.now();
+		// A previous serialized cache has the most recent items first
+		for (var l = arr.length - 1; l >= 0; l--) {
+			var hit = arr[l];
+			var expiresAt = hit.e || 0;
+			if (expiresAt === 0) {
+				// the item was created without expiration in a non aged cache
+				this.set(hit.k, hit.v);
+			} else {
+				var maxAge = expiresAt - now;
+				// dont add already expired items
+				if (maxAge > 0) {
+					this.set(hit.k, hit.v, maxAge);
+				}
+			}
+		}
+	};
 
-LRUCache.prototype.load = function (arr) {
-  // reset the cache
-  this.reset()
+	LRUCache.prototype.prune = function () {
+		var self = this;
+		this[CACHE].forEach(function (value, key) {
+			get(self, key, false);
+		});
+	};
 
-  var now = Date.now()
-  // A previous serialized cache has the most recent items first
-  for (var l = arr.length - 1; l >= 0; l--) {
-    var hit = arr[l]
-    var expiresAt = hit.e || 0
-    if (expiresAt === 0) {
-      // the item was created without expiration in a non aged cache
-      this.set(hit.k, hit.v)
-    } else {
-      var maxAge = expiresAt - now
-      // dont add already expired items
-      if (maxAge > 0) {
-        this.set(hit.k, hit.v, maxAge)
-      }
-    }
-  }
-}
+	function get(self, key, doUse) {
+		var hit;
+		var node = self[CACHE].get(key);
+		if (node) {
+			hit = node.value;
+			if (isStale(self, hit)) {
+				del(self, node);
+				if (!self[ALLOW_STALE]) {
+					hit = undefined;
+				}
+			} else {
+				if (doUse) {
+					self[LRU_LIST].unshiftNode(node);
+				}
+			}
+			if (hit) {
+				hit = hit.value;
+			}
+		}
+		return hit;
+	}
 
-LRUCache.prototype.prune = function () {
-  var self = this
-  this[CACHE].forEach(function (value, key) {
-    get(self, key, false)
-  })
-}
+	function isStale(self, hit) {
+		if (!hit || (!hit.maxAge && !self[MAX_AGE])) {
+			return false;
+		}
+		var stale = false;
+		var diff = Date.now() - hit.now;
+		if (hit.maxAge) {
+			stale = diff > hit.maxAge;
+		} else {
+			stale = self[MAX_AGE] && (diff > self[MAX_AGE]);
+		}
+		return stale;
+	}
 
-function get (self, key, doUse) {
-  var node = self[CACHE].get(key)
-  if (node) {
-    var hit = node.value
-    if (isStale(self, hit)) {
-      del(self, node)
-      if (!self[ALLOW_STALE]) hit = undefined
-    } else {
-      if (doUse) {
-        self[LRU_LIST].unshiftNode(node)
-      }
-    }
-    if (hit) hit = hit.value
-  }
-  return hit
-}
+	function trim(self) {
+		if (self[LENGTH] > self[MAX]) {
+			for (var walker = self[LRU_LIST].tail; self[LENGTH] > self[MAX] && walker !== null;) {
+				// We know that we're about to delete this one, and also
+				// what the next least recently used key will be, so just
+				// go ahead and set it now.
+				var prev = walker.prev;
+				del(self, walker);
+				walker = prev;
+			}
+		}
+	}
 
-function isStale (self, hit) {
-  if (!hit || (!hit.maxAge && !self[MAX_AGE])) {
-    return false
-  }
-  var stale = false
-  var diff = Date.now() - hit.now
-  if (hit.maxAge) {
-    stale = diff > hit.maxAge
-  } else {
-    stale = self[MAX_AGE] && (diff > self[MAX_AGE])
-  }
-  return stale
-}
+	function del(self, node) {
+		if (node) {
+			var hit = node.value;
+			if (self[DISPOSE]) {
+				self[DISPOSE](hit.key, hit.value);
+			}
+			self[LENGTH] -= hit.length;
+			self[CACHE].delete(hit.key);
+			self[LRU_LIST].removeNode(node);
+		}
+	}
 
-function trim (self) {
-  if (self[LENGTH] > self[MAX]) {
-    for (var walker = self[LRU_LIST].tail;
-         self[LENGTH] > self[MAX] && walker !== null;) {
-      // We know that we're about to delete this one, and also
-      // what the next least recently used key will be, so just
-      // go ahead and set it now.
-      var prev = walker.prev
-      del(self, walker)
-      walker = prev
-    }
-  }
-}
+	// classy, since V8 prefers predictable objects.
+	function Entry(key, value, length, now, maxAge) {
+		this.key = key;
+		this.value = value;
+		this.length = length;
+		this.now = now;
+		this.maxAge = maxAge || 0;
+	}
 
-function del (self, node) {
-  if (node) {
-    var hit = node.value
-    if (self[DISPOSE]) {
-      self[DISPOSE](hit.key, hit.value)
-    }
-    self[LENGTH] -= hit.length
-    self[CACHE].delete(hit.key)
-    self[LRU_LIST].removeNode(node)
-  }
-}
-
-// classy, since V8 prefers predictable objects.
-function Entry (key, value, length, now, maxAge) {
-  this.key = key
-  this.value = value
-  this.length = length
-  this.now = now
-  this.maxAge = maxAge || 0
-}
-
-return LRUCache;
+	return LRUCache;
 }());
